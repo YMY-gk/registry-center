4389bd9e9f17edf8edeb6558a73a17b1ec64acb2
guokui <15637521598@163.com>
2023-03-25 20:24:09 +0800

    所属

---
+++ src/main/java/com/registry/ErukaApplication.java
@@ -0,0 +1,20 @@
+package com.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *@ClassName ErukaApplication
+ *@Deacription TODO
+ *@Author os-guokui
+ *@Date 2021/3/29 17:26
+ *@Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class ErukaApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(ErukaApplication.class, args);
+        }
+}
